cf127cb1a39dd957da2c93ea5a9194a9ae137c78
Nezuloxx <123016091+NezuloxxJW@users.noreply.github.com>
2024-10-31 16:13:23 +0100

    Update installPackage.sh

--- installation/installPackage.sh
+++ installation/installPackage.sh
@@ -2,4 +2,4 @@ sudo apt update
 sudo apt install -y python3 python3-pip mosquitto sqlite3 nginx --fix-missing
 pip -version
 
-sudo pip install nfcpy paho-mqtt waitress flask flask_cors flask_limiter --break-system-packages
+sudo pip install bcrypt nfcpy paho-mqtt waitress flask flask_cors flask_limiter --break-system-packages
