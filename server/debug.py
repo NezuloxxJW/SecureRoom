c972e33cd012cabb1a5d58ad8ef0a2c8c1bd901c
Nezuloxx <123016091+NezuloxxJW@users.noreply.github.com>
2024-11-06 15:40:27 +0100

    Update debug.py

--- server/debug.py
+++ server/debug.py
@@ -1,8 +1,7 @@
 __DEBUG__ = True
 def DbgPrint(string: str, color: str = "default"):
-    # Define color codes
     colors = {
-        "default": "\033[0m",  # Default color
+        "default": "\033[0m", 
         "red": "\033[31m",
         "green": "\033[32m",
         "yellow": "\033[33m",
@@ -11,10 +10,7 @@ def DbgPrint(string: str, color: str = "default"):
         "cyan": "\033[36m",
         "white": "\033[37m",
     }
-
-    # Get the color code, default to "default" if not recognized
     color_code = colors.get(color, colors["default"])
     
-    # Print the colored string
     if __DEBUG__:
-        print(f"{color_code}{string}\033[0m")  # Reset color after printing
+        print(f"{color_code}{string}\033[0m")
