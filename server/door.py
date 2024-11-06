65a93a908e8fb8d0c9930aafb615560f7791d27d
Nezuloxx <123016091+NezuloxxJW@users.noreply.github.com>
2024-11-06 15:36:52 +0100

    Update door.py

--- server/door.py
+++ server/door.py
@@ -1,30 +1,22 @@
 import paho.mqtt.client as mqtt
-
 from debug import DbgPrint
 
 nuki_ids = ['3AD7396C']
 
-
+client = None
 
 class door:
-    def open():
+    def init():
+        global client
         client = mqtt.Client()
         username = "admin_eptm"
         password = "12345-"
         client.username_pw_set(username, password)
         client.connect("192.168.0.10", 1883, 60)
+
+    def open():
+        door.init()
         action_topic = f"nuki/{nuki_ids[0]}/lockAction"
         client.publish(action_topic, "1")
         client.disconnect()
         DbgPrint("[+] Ouverture envoyée","green")
-
-    def close():
-        client = mqtt.Client()
-        username = "admin_eptm"
-        password = "12345-"
-        client.username_pw_set(username, password)
-        client.connect("192.168.0.10", 1883, 60)
-        action_topic = f"nuki/{nuki_ids[0]}/lockAction"
-        client.publish(action_topic, "2") 
-        client.disconnect()
-        DbgPrint("[+] Fermeture envoyée", "green")
